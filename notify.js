dcd974c8916f87f557157009974190b4fb6c8871
duschsba <sebastian.duschinger@coi.de>
2013-04-04 23:36:24 +0200

    Switched from local storage to remote storage

--- notify.js
+++ notify.js
@@ -8,20 +8,31 @@ function NotifyAlbum(artist, albumtitle) {
 			
 }
 
+function User(userid, email) {
+	var self = this;
+	
+	self.userid = userid;
+	self.email = email;
+}
+
+function StorageObject(user, albums) {
+	var self = this;
+	self.userid = user.userid;
+	self.email = user.email;
+	self.albums = albums;
+	
+}
 
 function NotifyCtrl($scope) {
-	$scope.username = "Duschi";
-	$scope.albums = [new NotifyAlbum("Bonobo", "Northern Borders"),
+	$scope.currentuser = new User("duschsba", "sebastian.duschinger@gmail.com");
+	/*$scope.albums = [new NotifyAlbum("Bonobo", "Northern Borders"),
 		new NotifyAlbum("The Strokes", "Comedown Machine"),
-	];
-	
-	$scope.loadData = function() {
-		$scope.load();
-		/*$.getJSON("https://dl.dropbox.com/u/1265799/data.json?jsoncallback=?", function(data) {
-			alert("Hurz");
-			$scope.albums = json;
-		}).error(function() { alert("error"); });*/
+	];*/
 		
+	$scope.loadData = function() {
+		console.log("Current user: " + $scope.currentuser.userid);
+		console.log("Current user email: " + $scope.currentuser.email);
+		$scope.loadRemote();		
 	}
 	
 	$scope.addAlbum = function() {
@@ -32,25 +43,44 @@ function NotifyCtrl($scope) {
 		$scope.refresh();
 		$('#inputArtist').focus();
 	}
-  	
+	
 	$scope.save = function() {
+		$scope.saveRemote();
+		//$scope.saveLocal();
+	}
+  	
+	$scope.saveLocal = function() {
 		localStorage.notifyAlbums = JSON.stringify($scope.albums);
 	}
 	
-	$scope.load = function() {
+	$scope.loadLocal = function() {
 		$scope.albums = JSON.parse(localStorage.notifyAlbums);
 		$scope.refresh();
 	}
-  
-  $scope.saveMongo = function() {
     
-    $.ajax('http://localhost:8080/albums', {
-      data : JSON.stringify($scope.albums),
+  $scope.saveRemote = function() {
+	var so = new StorageObject($scope.currentuser, $scope.albums)
+		
+    $.ajax('http://localhost:8080/user?callback=?', {
+      data : JSON.stringify(so),
       contentType : 'application/json',
       type : 'POST'
-      });
+      })  
   }
-	
+  
+    $scope.loadRemote = function() {
+	 $.getJSON('http://localhost:8080/user/' + $scope.currentuser.userid, function(data) {
+		//$scope.albums = data[0].albums;
+		$scope.albums = [];
+		$.each(data[0].albums, function(idx){
+			var d = data[0].albums[idx];
+			var newAlbum = new NotifyAlbum(d.artist, d.albumtitle);
+			$scope.albums.push(newAlbum);
+		});
+		$scope.refresh();
+	 });
+  }
+  
 	$scope.delete = function(idx) {
 		$scope.albums.splice(idx,1);
 		$scope.save();
@@ -59,16 +89,17 @@ function NotifyCtrl($scope) {
 	$scope.refresh = function() {
 		$.each($scope.albums, function(index, album) {
 			$.getJSON("http://ws.spotify.com/search/1/album?q=" + album.artist + " " + album.albumtitle, function(data) {
-				console.log(data);
+				
 				if (data.albums.length > 0) {
 					album.available = true;
 					album.url = "http://open.spotify.com/album/" + data.albums[0].href.split(":")[2];
 					$scope.$apply();
+					
 				}
-				//console.log(data.albums[0].name);
 			})			
 			
 		});
+		$scope.save()
 	}
 	
 }
